c450d534977a20ded4d8f5189bc7614f47fb08da
schm00g <hk8igureu@relay.firefox.com>
2022-05-04 15:38:57 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,12 @@ export default function Footer() {
   return (
     <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10">
       <a
-        href='https://www.pointer.gg?utm_source=solana-pay'
+        href='https://solana.com/developers/payments'
         target='_blank'
         rel='noopener noreferrer'
       >
-        Learn web3 dev and earn crypto rewards at{" "}
-        <span>Pointer</span>
+        Powered by {" "}
+        <span>Solana Payments</span>
       </a>
     </footer>
   )

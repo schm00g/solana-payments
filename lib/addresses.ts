c11f01ea60830c455f5c16be30af1474710f32eb
schm00g <hk8igureu@relay.firefox.com>
2022-05-06 12:33:07 +0100

    public address for payee

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,3 +1,5 @@
 import { PublicKey } from "@solana/web3.js"
 
 export const shopAddress = new PublicKey('7YrUAbivfpiT6t4Wx2jLLWKiG1zFoEhHTnVurHF1vEsN');
+
+export const usdcAddress = new PublicKey('66ETnUrhveBbUN5gS3cFhebvYBWtH34zRfMXM9imPgaw');
